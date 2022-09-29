d506e1b5dd60e720dd04ffd41caa5bf2f6ff021f
Ilyagavrilin <gavrilin.id@phystech.edu>
2022-09-29 23:48:47 +0300

    update ideal cache algorithm
    time used for count cache hits decreased

--- LRU_2Q_cache/ideal_cache.hpp
+++ LRU_2Q_cache/ideal_cache.hpp
@@ -17,25 +17,53 @@ template <typename T, typename keyT = int> struct page_t_ {
 template <typename T, typename keyT = int, typename DstT = unsigned long> struct cache_t {
     using page_t = page_t_<T>;
     using ReqIt = typename std::list<keyT>::iterator;
-    std::list<keyT> &requests;
-    size_t size, cur_size;
+    struct req_t {
+        keyT key;
+        unsigned long dst_to_next;
+        
+        req_t(): key(0), dst_to_next(0) {};
+        req_t(keyT key_, unsigned long dst_): key(key_), dst_to_next(dst_) {};
+    };
+    req_t* reqs;
+    size_t size, cur_size, reqs_cnt;
     std::multimap<DstT, page_t> cache;
-    using MapIt = typename std::multimap<DstT, page_t>::iterator;
+    using MapIt = typename std::map<DstT, page_t>::iterator;
     std::unordered_map<keyT, MapIt> hash;
 
     
     
-    cache_t(std::list<keyT> &req, size_t sz): requests(req) {
-        size = sz;
-        cur_size = 0;
+    cache_t(std::list<keyT> &requests, size_t sz, size_t nreqs): cur_size(0), size(sz), reqs_cnt(nreqs) {
+        std::map<keyT, unsigned long> elems;
+        reqs = new req_t[nreqs];
+        unsigned long pos = 0;
+        for (keyT req: requests) {
+            auto hit = elems.find(req);
+            if (hit == elems.end()) {
+                elems[req] = pos;
+            } else {
+                reqs[hit->second].key = hit->first;
+                reqs[hit->second].dst_to_next = pos - hit->second;
+                elems[req] = pos;
+            }
+
+            pos++;
+        }
+        for (const auto& [key, value] : elems) {
+            reqs[value].key = key;
+            reqs[value].dst_to_next = ULONG_MAX;
+        }
+
     };
     
-    unsigned long count_length(ReqIt cur_req, unsigned long offset) {
-        ReqIt elem;
-        keyT val = *cur_req;
-        elem = std::find(++cur_req, requests.end(), val);
-        if (elem == requests.end()) return ULONG_MAX;
-        else return std::distance(cur_req, elem) + 1 + offset;
+    void dump_reqs() const {
+        for (int i = 0; i < reqs_cnt; i++) {
+            std::cout << "{req: " << reqs[i].key << ", distance:" << reqs[i].dst_to_next << "}; ";
+         }
+    }
+    
+    unsigned long count_length(unsigned long distance, unsigned long offset) {
+        if (distance == ULONG_MAX) return ULONG_MAX;
+        else return distance + offset;
     }
 
     bool full() const{
@@ -47,8 +75,17 @@ template <typename T, typename keyT = int, typename DstT = unsigned long> struct
         return a.dst_to_next < b.dst_to_next;
     };
     
-    bool add_req(unsigned long nreq, ReqIt& cur_req, T(*slow_get_page)(keyT)) {
-        auto hit = hash.find(*cur_req);
+    void dump() const {
+        std::cout << "current size: " << cur_size << " ";
+        std::cout << "maximum size:" << size << std::endl;
+        for (auto elem = cache.begin(); elem != cache.end(); elem++) {
+            std::cout << "{ key: " << elem->first << ", elem: " << elem->second.key << "}; ";
+        }
+        std::cout << std::endl;
+    }
+    
+    bool add_req(unsigned long nreq, req_t cur_req, T(*slow_get_page)(keyT)) {
+        auto hit = hash.find(cur_req.key);
         if (hit == hash.end()) {
             if (full()) {
                 MapIt to_delete = std::prev(cache.end());
@@ -56,34 +93,31 @@ template <typename T, typename keyT = int, typename DstT = unsigned long> struct
                 cache.erase(to_delete);
                 cur_size--;
             }
-            page_t to_add(slow_get_page(*cur_req), *cur_req);
-            MapIt added = cache.insert(std::pair{count_length(cur_req, nreq), to_add});
-            hash[*cur_req] = added;
+            page_t to_add(slow_get_page(cur_req.key), cur_req.key);
+            MapIt added = cache.insert(std::pair{count_length(cur_req.dst_to_next, nreq), to_add});
+            hash[cur_req.key] = added;
             cur_size++;
             return false;
         } else {
             MapIt hit_map = hit->second;
             page_t hit_page = hit_map->second;
             cache.erase(hit_map);
-            MapIt added = cache.insert(std::pair{count_length(cur_req, nreq), hit_page});
-            hash[*cur_req] = added;
-            cur_size++;
+            MapIt added = cache.insert(std::pair{count_length(cur_req.dst_to_next, nreq), hit_page});
+            hash[cur_req.key] = added;
             return true;
         }
 
         
     }
     int hit_cnt(T(*slow_get_page)(keyT)) {
-        int hits = 0, nreq = 0;
-        for (auto key = requests.begin(); key != requests.end(); key++) {
-            if (add_req(nreq, key, slow_get_page)) {
+        int hits = 0;
+        for (unsigned long nreq = 0; nreq < reqs_cnt; nreq++) {
+            if (add_req(nreq, reqs[nreq], slow_get_page)) {
                 hits++;
-                
+            }    
 #ifdef DEBUG 
-                dump();
+            dump();
 #endif                
-            }
-            nreq++;
             
         }
         return hits;         
